8b6d1a8d28ed72620497875da3f633e18a69ff6c
Viishwa Chauhan <vishwa0804@gmail.com>
2024-02-19 14:37:45 +0530

    add program

---
+++ pro-5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void pattern(){
+	printf("\n* * * ");
+	printf("\n* * *\t\t* * *");
+	printf("\n* * *\t\t  * * *");
+	printf("\n*\t\t* * * * *");
+	printf("\n*\t\t  * * * * *");
+	printf("\n*\t\t    * * * * *");
+	printf("\n*\t\t      * * *");
+	printf("\n*\t\t      * *");
+	printf("\n*\t\t      *");
+	printf("\n* * *\t	* * *");
+	printf("\n* * *");
+	
+}
+
+void main(){
+	pattern();
+}
